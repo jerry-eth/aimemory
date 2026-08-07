fce1e3f217e5d85fea5709ce012ddd606c7e5a05
jerry <jerry@yklh.top>
2026-08-07 17:02:01 +0800

    修复 MemoryMonitorService:Start() 使用构造函数传入的 intervalMs,RecentPercents 返回锁内快照并改用私有锁对象

--- src/AiMemoryManager/Services/MemoryMonitorService.cs
+++ src/AiMemoryManager/Services/MemoryMonitorService.cs
@@ -9,22 +9,25 @@ public class MemoryMonitorService : IDisposable
     private readonly INativeMemoryApi _native;
     private readonly Timer _timer;
     private readonly List<double> _recent = new();
+    private readonly object _sync = new();
+    private readonly int _intervalMs;
 
     public event EventHandler<SystemMemoryInfo>? Sampled;
-    public IReadOnlyList<double> RecentPercents => _recent;
+    public IReadOnlyList<double> RecentPercents { get { lock (_sync) return _recent.ToArray(); } }
 
     public MemoryMonitorService(INativeMemoryApi native, int intervalMs = 2000)
     {
         _native = native;
+        _intervalMs = intervalMs;
         _timer = new Timer(_ => Sample(), null, Timeout.Infinite, intervalMs);
     }
 
-    public void Start() => _timer.Change(0, 2000);
+    public void Start() => _timer.Change(0, _intervalMs);
 
     private void Sample()
     {
         var info = _native.GetSystemMemory();
-        lock (_recent)
+        lock (_sync)
         {
             _recent.Add(info.UsedPercent);
             if (_recent.Count > MaxSamples) _recent.RemoveAt(0);
